5af1e8821c30db742bb49c98cfbbe8f1b47f59ec
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-06-13 20:42:15 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,5 @@
+a = 10
+b = 2
+c = a * b
+
+print("MULTIPLICATION OF "+str(a)+" AND "+str(b)+" IS "+str(c))
---
+++ variables.py
@@ -0,0 +1,21 @@
+a1a = 10
+print(a1a)
+
+_firstnumber = 100
+print(_firstnumber)
+
+_ = 500
+print(_)
+
+# print = 200
+# print(print)
+
+n = 2000
+N = 400
+n = 800
+print(n)
+print(N)
+
+firstnumber = 100
+secondnumber = 120
+result = firstnumber + secondnumber
