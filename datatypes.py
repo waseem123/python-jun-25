1eef400b99882df2fc82bde8caea7d81c42bd178
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-06-16 20:08:17 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+x = "Python is simple"
+print(x)
+print(type(x))
+
+x = 1000
+print(x)
+print(type(x))
+
+x = 10.65
+print(x)
+print(type(x))
+
+x = 10+6j
+print(x)
+print(type(x))
+
+x = [100,200,300,400,500]
+print(x)
+print(type(x))
+
+x = (30,40,50,60)
+print(x)
+print(type(x))
+
+x = {"c",'C++','Java','Python','C++'}
+print(x)
+print(type(x))
+
+x = {'name':'Waseem','city':'Solapur','state':'Maharashtra'}
+print(x)
+print(type(x))
+
+x = False
+print(x)
+print(type(x))
